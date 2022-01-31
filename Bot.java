aec29d9d5f79eddf6063ce18951af9a99c39c6b1
wczhang04 <86317881+wczhang04@users.noreply.github.com>
2022-01-31 07:57:43 -0800

    Add files via upload

--- Bot.java
+++ Bot.java
@@ -30,29 +30,33 @@ public class Bot extends TeamRobot
 	 * Parts from Sample Robot TrackFire
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		
-		// Find position of scanned Robot
-		double absoluteBearing = getHeading() + e.getBearing();
-		double gunBearing = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-		double distance = e.getDistance();
-		// If accurate enough and within distance of 500, shoot hard with 3 
-		if (Math.abs(gunBearing) <= 3 && e.getDistance() < 500) {
-			turnGunRight(gunBearing);
-			fire(3);
+	// Don't fire at teammates! Returns true if scanned teammate
+		if(isTeammate(e.getName())) { 
+			return;
 		}
-		// If accurate enough but at distance greater than 500, shoot lighter with 1
-		else if (Math.abs(gunBearing) <= 3 && e.getDistance() > 500) {
-			turnGunRight(gunBearing);
-			fire(1);
-		}
-		// Otherwise just scan
-		else {
-			turnGunRight(gunBearing);
-		}
-		// Generates another scan event if we see a robot
-		if (gunBearing == 0) {
-			scan();
+		if(isTeammate(e.getName()) == false) {
+			// Find position of scanned Robot
+			double absoluteBearing = getHeading() + e.getBearing();
+			double gunBearing = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+			double distance = e.getDistance();
+			// If accurate enough and within distance of 500, shoot hard with 3 
+			if (Math.abs(gunBearing) <= 3 && e.getDistance() < 500) {
+				turnGunRight(gunBearing);
+				fire(3);
+			}
+			// If accurate enough but at distance greater than 500, shoot lighter with 1
+			else if (Math.abs(gunBearing) <= 3 && e.getDistance() > 500) {
+				turnGunRight(gunBearing);
+				fire(1);
+			}
+			// Otherwise just scan
+			else {
+				turnGunRight(gunBearing);
+			}
+			// Generates another scan event if we see a robot
+			if (gunBearing == 0) {
+				scan();
+			}
 		}
 	}
 	/**
